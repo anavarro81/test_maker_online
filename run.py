06e0a29d12ccf296ef0314b7ca237c3a8eaf5b4b
anavarro81 <anavarro67@hotmail.com>
2023-04-14 20:49:09 +0200

    tercer commit

--- run.py
+++ run.py
@@ -12,6 +12,22 @@ preguntas = [
     '¿Cuá es la capital de Alemania?',
     ]
 
+opciones = [
+    ['Madrid', 'Lisboa', 'París', 'Berlín'],
+    ['Madrid', 'Lisboa', 'París', 'Berlín'],
+    ['Madrid', 'Lisboa', 'París', 'Berlín'],
+    ['Madrid', 'Lisboa', 'París', 'Berlín'],
+]
+
+respuestas = [
+    ['c', 'a', 'b', 'd']
+]
+
+user_aswer = []
+
+key_values = ['a', 'b', 'c', 'd']
+
+
 tot_preg = len(preguntas) - 1
 
 print (f'Total preguntas = {tot_preg}')
@@ -24,6 +40,8 @@ habilitado = {'prev': "",
 
 def estado_nav(page):
 
+    print ('->estado_nav | page = {p} ')
+
     if page == 1:
        habilitado['prev'] = 'disabled'
        habilitado['next'] = ''
@@ -35,31 +53,44 @@ def estado_nav(page):
 
 @app.route('/')
 def index():
-    return render_template('index.html', page=0)
+    return render_template('index.html')
 
 
 @app.route('/siguiente', methods=['GET', 'POST'])
 def siguiente():
 
-    page = int (request.args.get('page')) 
-    habilitado = estado_nav(page)
+    if request.method == 'POST':        
+        page = int (request.form['page'])
 
-    if page <= tot_preg:
-        pregunta = preguntas[page]        
-    else: 
-        return render_template('final_test.html')
+        
 
-    return render_template('siguiente.html', pregunta=pregunta, page=page, habilitado=habilitado)
+        print (f'/siguiente | page = {page}')
+
+        # Si vengo de index (st)
+        if 'start_game' in request.form or 'siguiente'  in request.form:
+            page      = page + 1        
+        elif 'anterior' in request.form:            
+            page = page - 1
+
+        habilitado = estado_nav(page)
+
+        # Guardo la opción dada por el usuario: 
+        if 'opcion' in request.form:
+            opc = request.form['opcion']           
+            user_aswer.insert(page, opc)
+        
+        
+        
+        if page <= tot_preg:
+            pregunta = preguntas[page]
+            dict_opciones = dict(zip(key_values, opciones[page-1]))         
+        else: 
+            return render_template('final_test.html')
+
+        
+
+    return render_template('siguiente.html', pregunta=pregunta, page=page, habilitado=habilitado, dict_opciones=dict_opciones)
     
-@app.route('/anterior', methods=['GET', 'POST'])
-def anterior():
-
-    page = int (request.args.get('page'))       
-    pregunta = preguntas[page]
-    habilitado = estado_nav(page)
-
-    return render_template('siguiente.html', pregunta=pregunta, page=page, habilitado=habilitado)
-
 
 if __name__ == '__main__':
     app.config.from_object(config['development'])
