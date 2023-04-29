483e54eb8cf4da210ea62d05b4e1b5416c5c71d8
anavarro81 <anavarro67@hotmail.com>
2023-04-29 12:00:22 +0200

    incluir variables de entorno

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
+
 class Config:
     SECRET_KEY = "*W8Tr^g9UyM!sc8Uuuuc"
 
@@ -6,10 +8,24 @@ class Config:
 class DevelopmentConfig(Config):
     DEBUG = True
     # Parametros de conexion con la BBDD.
-    MYSQL_HOST = "localhost"
-    MYSQL_USER = "root"
-    MYSQL_PASSWORD = ""
-    MYSQL_DB = "preguntas"
+    # MYSQL_HOST = "localhost"
+    # MYSQL_USER = "root"
+    # MYSQL_PASSWORD = ""
+    # MYSQL_DB = "preguntas"
+
+    # # Parametros de conexion con la BBDD.
+    MYSQL_HOST = os.environ.get('MYSQL_HOST')
+    MYSQL_USER = os.environ.get('MYSQL_USER')
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD')    
+    # MYSQL_PASSWORD = ""
+    MYSQL_DB = os.environ.get('MYSQL_DB')
+
 
 
 config = {"development": DevelopmentConfig}
+
+my_config = DevelopmentConfig
+print (f'MYSQL_HOST = {my_config.MYSQL_HOST}') 
+print (f'MYSQL_USER = {my_config.MYSQL_USER}')
+print (f'MYSQL_PASSWORD = {my_config.MYSQL_PASSWORD}')
+print (f'MYSQL_DB = {my_config.MYSQL_DB}')
